edbbd64c7636722caccf897bc2bec4c9549b733b
Michael Reddick <michael.reddick@gmail.com>
2021-11-14 01:42:58 +0000

    Use neovim nightly overlay

--- flake.nix
+++ flake.nix
@@ -3,9 +3,21 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
+
+    # make home-manager use same nixpkgs we're using
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   };
   
-  outputs = inputs: {
+  outputs = { self, ... }@inputs:
+    let
+      overlays = [
+        inputs.neovim-nightly-overlay.overlay
+      ];
+    in
+
+    {
     homeConfigurations = {
       wsl = inputs.home-manager.lib.homeManagerConfiguration {
         system = "x86_64-linux";
@@ -13,13 +25,20 @@
         username = "michaelr";
 
         configuration = { config, lib, pkgs, ... }: {
+
           xdg.configFile."nix/nix.conf".text = ''
             experimental-features = nix-command flakes ca-references
           '';
+
           nixpkgs.config = { allowUnfree = true; };
+	  nixpkgs.overlays = overlays;
 
           # Let Home Manager install and manage itself.
           programs.home-manager.enable = true;
+
+          home.packages = with pkgs; [
+            openssh 
+          ];
 
           programs.git = {
             enable = true;
