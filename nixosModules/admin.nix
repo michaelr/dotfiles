6a6fbb1683c38fe1bd0bbd945e258afdee02b318
Michael Reddick <michael.reddick@gmail.com>
2022-08-30 20:47:55 -0500

    trusted-users rename

--- nixosModules/admin.nix
+++ nixosModules/admin.nix
@@ -9,5 +9,5 @@
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAOst/A8NYnpPWB2Lg/aPdFfzyBbp221H+RZ6qMFccEC michael.reddick@gmail.com" ];
   };
   security.sudo.wheelNeedsPassword = false;
-  nix.trustedUsers = [ "@wheel" ]; # https://github.com/serokell/deploy-rs/issues/25
+  nix.settings.trusted-users = [ "@wheel" ]; # https://github.com/serokell/deploy-rs/issues/25
 }
