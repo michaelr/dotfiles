c7a9f07d8e0e5a1a9ce2b674671d62fff9cc79b0
Michael Reddick <michael.reddick@gmail.com>
2022-09-25 20:00:05 -0500

    update treesitter grammars

--- modules/nvim/treesitter.lua
+++ modules/nvim/treesitter.lua
@@ -1,10 +1,10 @@
 require 'nvim-treesitter.configs'.setup {
     -- Note: installing nix grammer requires treesitter installed as command line too
     ensure_installed = {
-        "bash", "c", "css", "dockerfile", "elixir", "go", "haskell", "html",
-        "java", "javascript", "json", "latex", "lua", "nix", "python",
-        "regex", "ruby", "rust", "scss", "toml", "tsx", "typescript", "yaml",
-        "heex"
+        "bash", "c", "css", "dockerfile", "elixir", "fish", "go", "heex",
+        "help", "html", "javascript", "json", "lua", "make", "markdown",
+        "nix", "perl", "python", "regex", "ruby", "scss", "sql", "toml",
+        "tsx", "typescript", "vim", "yaml"
     },
     highlight = {
         enable = true,
