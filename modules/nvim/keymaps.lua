6e7bf00fa18be10052a005de80b957e44bfc88f0
Michael Reddick <michael.reddick@gmail.com>
2022-10-27 17:53:08 -0600

    manually define keymaps i was using in whichkey

--- modules/nvim/keymaps.lua
+++ modules/nvim/keymaps.lua
@@ -1,2 +1,16 @@
-vim.keymap.set('', '<F6>', '<cmd>Dispatch browser-sync start --server --files "*.js, *.html, *.css"<CR>')
-vim.keymap.set('', '<F7>', '<cmd>Dispatch npm run dev<CR>')
+local keymap = vim.keymap.set
+
+keymap('', '<F6>', '<cmd>Dispatch browser-sync start --server --files "*.js, *.html, *.css"<CR>')
+keymap('', '<F7>', '<cmd>Dispatch npm run dev<CR>')
+keymap({ 'n', 'v', }, '<leader>e', '<cmd>NvimTreeToggle<cr>')
+keymap({ 'n', 'v', }, "<leader>bd", "<cmd>bd<CR>")
+keymap('', "<leader>sb", "<cmd>Telescope buffers<CR>")
+keymap('', "<leader>sc", "<cmd>Telescope command_history<CR>")
+keymap('', "<leader>sC", "<cmd>Telescope commands<CR>")
+keymap('', "<leader>sf", "<cmd>Telescope find_files<CR>")
+keymap('', "<leader>sh", "<cmd>Telescope help_tags<CR>")
+keymap('', "<leader>sm", "<cmd>Telescope marks<CR>")
+keymap('', "<leader>sM", "<cmd>Telescope man_pages<CR>")
+keymap('', "<leader>sr", "<cmd>Telescope registers<CR>")
+keymap('', "<leader>st", "<cmd>Telescope live_grep<CR>")
+keymap('', "<leader>sT", "<cmd>TodoTelescope<CR>")
