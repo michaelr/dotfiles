497106fee1d167f6d4aac7b2bdd7f061ff1b2e6e
Michael Reddick <michael.reddick@gmail.com>
2022-10-31 09:40:43 -0500

    change timezone

--- machines/nixos-vmware/default.nix
+++ machines/nixos-vmware/default.nix
@@ -111,8 +111,10 @@ in
 
   virtualisation.vmware.guest.enable = true;
 
+  # timedatectl list-timezones
+  time.timeZone = "America/Bogota";
   # time.timeZone = "America/Chicago";
-  time.timeZone = "America/Denver";
+  # time.timeZone = "America/Denver";
 
   users.mutableUsers = false;
   users.groups.plocate = { };
